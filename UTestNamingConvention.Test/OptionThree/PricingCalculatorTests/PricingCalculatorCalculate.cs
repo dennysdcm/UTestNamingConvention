ca58083ed104b64fc4e45a2794e775a166eee123
dennysdcm <dcm.dennys.dev@gmail.com>
2021-03-24 18:07:28 -0400

    Add unit tests using a class file by method and ExpectedResultAndCondition naming convention

---
+++ UTestNamingConvention.Test/OptionThree/PricingCalculatorTests/PricingCalculatorCalculate.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UTestNamingConvention.Core;
+
+namespace UTestNamingConvention.Test.OptionThree.PricingCalculatorTests
+{
+    [TestClass]
+    public class PricingCalculatorCalculate
+
+    {
+        private Customer customer;
+        private PricingCalculator pricingCalculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            pricingCalculator = new PricingCalculator();
+        }
+
+        [TestMethod]
+        public void ReturnPriceLessPreferredCustomerDiscountGivenPreferredCustomerAndQuatityGreatherThanOne()
+        {
+            // Arrange
+            var units = 1;
+            var unitPrice = 2.5m;
+            customer = new Customer { IsPreferred = true };
+
+            // Act
+            var calculatedPrice = pricingCalculator.Calculate(units, customer, unitPrice);
+
+            // Assert
+            var expectedPrice = unitPrice * units * (1 - PricingCalculator.PreferredCustomerDiscount);
+            Assert.AreEqual(expectedPrice, calculatedPrice);
+        }
+
+        [TestMethod]
+        public void ReturnPriceTimesQuantityGivenRegularCustomerAndQuantityGreaterThanOne()
+        {
+            // Arrange
+            var units = 1;
+            var unitPrice = 2.5m;
+            customer = new Customer { IsPreferred = false };
+
+            // Act
+            var calculatedPrice = pricingCalculator.Calculate(units, customer, unitPrice);
+
+            // Assert
+            var expectedPrice = unitPrice * units;
+            Assert.AreEqual(expectedPrice, calculatedPrice);
+        }
+
+        [TestMethod]
+        public void ReturnZeroGivenQuantityZero()
+        {
+            // Arrange
+            var units = 0;
+            var unitPrice = 2.5m;
+            customer = new Customer { IsPreferred = true };
+
+            // Act
+            var calculatedPrice = pricingCalculator.Calculate(units, customer, unitPrice);
+
+            // Assert
+            var expectedPrice = 0m;
+            Assert.AreEqual(expectedPrice, calculatedPrice);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionGivenQuantityLessThanZero()
+        {
+            // Arrange
+            var units = -1;
+            var unitPrice = 2.5m;
+            customer = new Customer { IsPreferred = true };
+
+            // Act
+            var calculatedPrice = pricingCalculator.Calculate(units, customer, unitPrice);
+
+            // Assert
+            Assert.Fail("Should have thrown an exception");
+        }
+    }
+}
